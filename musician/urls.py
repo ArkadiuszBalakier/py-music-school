f432aed8bb00f474176a31e3197707f106ef8fc4
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-10 08:53:28 +0100

    refactor: add basename "manage" in url for musicians

--- musician/urls.py
+++ musician/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from musician.views import MusicianViewSet
 
 router = routers.DefaultRouter()
-router.register(r'musicians', MusicianViewSet)
+router.register(r'musicians', MusicianViewSet, basename='manage')
 urlpatterns = [
     path("", include(router.urls)),
 ]
